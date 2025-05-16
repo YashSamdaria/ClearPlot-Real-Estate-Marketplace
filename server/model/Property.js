f70fd0ef8ef138207c3dd33daf0d94c856d765b5
unknown <yashsamdaria@gmail.com>
2025-05-16 22:49:34 +0530

    Updated Mongo Property model

--- server/model/Property.js
+++ server/model/Property.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const propertySchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   ListingType: { type: String, enum: ["Buy", "Rent"], required: true },
-  PropertyType: { type: String, enum: ["Apartment", "Standalone", "Villa", "Plot"], required: true },
+  PropertyType: { type: String, enum: ["Apartment", "Standalone", "Villa", "Row House", "Plot", "Farmhouse", "Penthouse", "Duplex House", "Loft", "Cottage", "Studio"], required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   City: String,
   Area: Number,
